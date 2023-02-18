d992324a67f62b79e72c319500f259b72961c275
Snoodelz <ludwig.mostrom@gmail.com>
2023-02-18 19:42:12 +0100

    Fixed some bugs

--- components/navbar.js
+++ components/navbar.js
@@ -4,73 +4,73 @@ import { useEffect } from "react";
 import { themeChange } from "theme-change";
 
 export default function Navbar() {
-  useEffect(() => {
-    themeChange(false);
-    // 👆 false parameter is required for react project
-  }, []);
-  return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex="0" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex="0"
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <Link href="/songs">
-                <a>Sånger</a>
-              </Link>
-            </li>
+	useEffect(() => {
+		themeChange(false);
+		// 👆 false parameter is required for react project
+	}, []);
+	return (
+		<div className="navbar bg-base-100">
+			<div className="navbar-start">
+				<div className="dropdown">
+					<label tabIndex="0" className="btn btn-ghost lg:hidden">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-5 w-5"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M4 6h16M4 12h8m-8 6h16"
+							/>
+						</svg>
+					</label>
+					<ul
+						tabIndex="0"
+						className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+					>
+						<li>
+							<Link href="/songs">
+								<a>Sånger</a>
+							</Link>
+						</li>
 
-            <li>
-              <select
-                data-choose-theme
-                className="select select-accent w-full max-w-xs"
-              >
-                <option value="light">Light</option>
-                <option value="dark">Dark</option>
-                <option value="cyberpunk">Cyberpunk</option>
-              </select>
-            </li>
-          </ul>
-        </div>
-        <Link href="/">
-          <a className="btn btn-ghost normal-case text-xl">Ingarö Moppers</a>
-        </Link>
-      </div>
-      <div className="navbar-end hidden lg:flex">
-        <ul className="menu menu-horizontal p-0">
-          <li>
-            <Link href="/songs">
-              <a>Sånger</a>
-            </Link>
-          </li>
+						<li>
+							<select
+								data-choose-theme
+								className="select select-accent w-full max-w-xs"
+							>
+								<option value="light">Light</option>
+								<option value="dark">Dark</option>
+								<option value="cyberpunk">Cyberpunk</option>
+							</select>
+						</li>
+					</ul>
+				</div>
+				<Link href="/">
+					<a className="btn btn-ghost normal-case text-xl">Ingarö Moppers</a>
+				</Link>
+			</div>
+			<div className="navbar-end hidden lg:flex">
+				<ul className="menu menu-horizontal p-0">
+					<li>
+						<Link href="/songs">
+							<a>Sånger</a>
+						</Link>
+					</li>
 
-          <li>
-            <select data-choose-theme className="">
-              <option value="light">Light</option>
-              <option value="dark">Dark</option>
-              <option value="cyberpunk">Cyberpunk</option>
-            </select>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+					<li>
+						<select data-choose-theme className="select pt-2.5">
+							<option value="light">Light</option>
+							<option value="dark">Dark</option>
+							<option value="cyberpunk">Cyberpunk</option>
+						</select>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -3,63 +3,51 @@ import Image from "next/image";
 import logo from "../img/moppersLogga.png";
 import Navbar from "../components/navbar";
 import styles from "../styles/Home.module.css";
+import Adrian from "../img/Adrian.jpg";
+import Albin from "../img/Albin.jpg";
+import David from "../img/David.jpg";
+import Felix from "../img/Felix.jpg";
+import Filip from "../img/Filip.jpg";
+import Gustav from "../img/Gustav.jpg";
+import Ludwig from "../img/Ludwig.jpg";
+import Max from "../img/Max.jpg";
+import Nazar from "../img/Nazar.jpg";
 
 export default function Home({ animals }) {
-  return (
-    <div>
-      <Navbar />
+	return (
+		<div>
+			<Navbar />
 
-      <main className=" max-w-4xl mx-auto">
-        <Image
-          src={logo}
-          objectPosition="center top"
-          alt="Moppers logo"
-          layout="intrinsic"
-        />
-        <em>We are moppers</em>
-        <div className="avatar-group -space-x-6">
-          <div className="avatar">
-            <div className="w-12">
-              <img src="https://scontent.fath3-4.fna.fbcdn.net/v/t1.6435-1/92951835_3680488471977552_4810835259576811520_n.jpg?stp=dst-jpg_p100x100&_nc_cat=108&ccb=1-7&_nc_sid=7206a8&_nc_ohc=ruCfrZFNxfYAX9M2v0-&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fath3-4.fna&oh=00_AT-YSr6ttG9f60jmWz5GIZ9vzLl3VNew2FayEmDhw9xMkw&oe=62E65308" />
-            </div>
-          </div>
-          <div className="avatar">
-            <div className="w-12">
-              <img src="https://scontent.fath3-3.fna.fbcdn.net/v/t1.6435-1/57393199_2158475394189249_5431572971468619776_n.jpg?stp=dst-jpg_p100x100&_nc_cat=105&ccb=1-7&_nc_sid=7206a8&_nc_ohc=vGLRssxjU0oAX-B-pZL&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fath3-3.fna&oh=00_AT_a20FyDyGqu6NnPiJeJpTp08034of7ON4tQvPRBVo-qA&oe=62E4358B" />
-            </div>
-          </div>
-          <div className="avatar">
-            <div className="w-12">
-              <img src="https://scontent.fath3-4.fna.fbcdn.net/v/t39.30808-1/291509474_5496676610353512_4252194792316095588_n.jpg?stp=dst-jpg_p100x100&_nc_cat=103&ccb=1-7&_nc_sid=7206a8&_nc_ohc=KSvNBymPjGUAX-u7OjC&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fath3-4.fna&oh=00_AT9fDhQcDzO0UNm5ELToAuF_ZyTf3I0qJ3yJGPzYgh9nug&oe=62C319B7" />
-            </div>
-          </div>
-          <div className="avatar">
-            <div className="w-12">
-              <img src="https://scontent.fath3-4.fna.fbcdn.net/v/t1.18169-1/389137_437735556265962_1696465757_n.jpg?stp=c218.38.474.473a_dst-jpg_s100x100&_nc_cat=103&ccb=1-7&_nc_sid=7206a8&_nc_ohc=fXn4UvGkVZEAX9U2gF7&_nc_oc=AQnobwNgZX1omM5VsRIzF1ZLwP6o_tLMOdavxmUl6OvQIG7yOVTxXSGJcnMt0qov5gk&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fath3-4.fna&oh=00_AT_1DsWD0RaYOQA7YEt4Au3iRyNgm0dJR2MY2xpcNLIWtg&oe=62E55C4C" />
-            </div>
-          </div>
-          <div className="avatar">
-            <div className="w-12">
-              <img src="https://scontent.fath3-4.fna.fbcdn.net/v/t1.18169-1/21077521_1125011200964086_3198769526153246647_n.jpg?stp=dst-jpg_p100x100&_nc_cat=101&ccb=1-7&_nc_sid=7206a8&_nc_ohc=U8Bf5vrN0_oAX8mZP2M&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fath3-4.fna&oh=00_AT_lFAH6YBXz-FLMuH_hTFA3GJGQe9vtgcRh_1msTYD0bw&oe=62E5E443" />
-            </div>
-          </div>
-          <div className="avatar">
-            <div className="w-12">
-              <img src="https://scontent.fath3-3.fna.fbcdn.net/v/t31.18172-1/11416405_1082238501827616_88997180176341473_o.jpg?stp=dst-jpg_p100x100&_nc_cat=109&ccb=1-7&_nc_sid=7206a8&_nc_ohc=xfdZ8aQFwxEAX_BP2kM&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fath3-3.fna&oh=00_AT9Tll98fpGCwydcrykTj23pv8rTxiswu8zqTNP9icVhIw&oe=62E650DE" />
-            </div>
-          </div>
-          <div className="avatar">
-            <div className="w-12">
-              <img src="https://scontent.fath3-4.fna.fbcdn.net/v/t1.6435-1/116890883_10158330208784333_8510656499278224746_n.jpg?stp=dst-jpg_p100x100&_nc_cat=102&ccb=1-7&_nc_sid=7206a8&_nc_ohc=kdd9UD1N-wgAX89fEVR&_nc_oc=AQktswh3tQjjzlmF51es56-NbUteFFOogOr5EB12HhVw-1XBxky7BZIpELIwJso2fXg&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fath3-4.fna&oh=00_AT_nnDg3cQBzu_lMB3O5rF-tHPRkf9awK2tDWuDtVwLTPg&oe=62E3292E" />
-            </div>
-          </div>
-          <div className="avatar">
-            <div className="w-12">
-              <img src="https://scontent.fath3-4.fna.fbcdn.net/v/t1.6435-1/67480225_2321304451283843_3744627525473533952_n.jpg?stp=cp0_dst-jpg_p80x80&_nc_cat=103&ccb=1-7&_nc_sid=7206a8&_nc_ohc=T1b6csDcI_UAX-7EyTs&_nc_ht=scontent.fath3-4.fna&oh=00_AT8tbsfoUSjH8d653pOOf8qbjXm1VxnjVPGiaKw06Oq54A&oe=62E58EB0" />
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+			<main className=" max-w-4xl mx-auto">
+				<Image
+					src={logo}
+					objectPosition="center top"
+					alt="Moppers logo"
+					layout="intrinsic"
+				/>
+				<em>We are moppers</em>
+				<div className="avatar-group -space-x-6">
+					<Avatar url={Adrian} namn={"Adrian"} />
+					<Avatar url={Albin} namn={"Albin"} />
+					<Avatar url={David} namn={"David"} />
+					<Avatar url={Felix} namn={"Felix"} />
+					<Avatar url={Filip} namn={"Filip"} />
+					<Avatar url={Gustav} namn={"Gustav"} />
+					<Avatar url={Max} namn={"Max"} />
+					<Avatar url={Nazar} namn={"Nazar"} />
+					<Avatar url={Ludwig} namn={"Ludwig"} />
+				</div>
+			</main>
+		</div>
+	);
+}
+
+function Avatar({ url, namn }) {
+	return (
+		<div className="avatar">
+			<div className="w-12 ">
+				<Image src={url} alt={namn} />
+			</div>
+		</div>
+	);
 }
